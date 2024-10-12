e878c94aba6be700e71868c61e82730e512a3e75
nobe4 <nobe4@users.noreply.github.com>
2024-10-12 18:27:54 +0200

    feat: new code

--- font/README.md
+++ font/README.md
@@ -1,1 +1,3 @@
 https://velvetyne.fr/download/?font=lineal
+
+Lineal by Frank Adebiaye, with the contribution of Anton Moglia, Ariel Martín Pérez. Distributed by velvetyne.fr.
--- main.go
+++ main.go
@@ -2,37 +2,68 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/tdewolff/canvas"
-	"github.com/tdewolff/canvas/renderers"
 	"github.com/tdewolff/canvas/renderers/pdf"
 )
 
 const lorem string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris. Vivamus hendrerit arcu sed`
 
+type FontMap struct {
+	name  string
+	style canvas.FontStyle
+}
+
 func main() {
 	fmt.Printf("Running at %s\n", time.Now().Format("2006-01-02 15:04:05"))
 
-	c := canvas.New(210, 297)
-	ctx := canvas.NewContext(c)
-
-	karrikFont := canvas.NewFontFamily("Karrik")
-	if err := karrikFont.LoadFontFile("./font/OTF/Lineal-Regular.otf", canvas.FontRegular); err != nil {
+	f, err := os.Create("test.pdf")
+	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
 
-	size := 30.0
-	for y := 297.0; y > 0; {
-		face := karrikFont.Face(size, canvas.Black, canvas.FontRegular, canvas.FontNormal)
+	font := canvas.NewFontFamily("Lineal")
+	pdf := pdf.New(f, 210, 297, &pdf.Options{})
 
-		txt := canvas.NewTextBox(face, lorem, 210.0, 0.0, canvas.Left, canvas.Top, 0.0, 0.0)
+	fontThicknesses := []FontMap{
+		{"Thin", canvas.FontThin},
+		{"Light", canvas.FontLight},
+		{"Regular", canvas.FontRegular},
+		{"Medium", canvas.FontMedium},
+		{"Bold", canvas.FontBold},
+		{"Black", canvas.FontBlack},
+		{"Heavy", canvas.FontExtraBold},
+	}
+
+	y := 297.0
+	for _, s := range fontThicknesses {
+		if err := font.LoadFontFile("./font/OTF/Lineal-"+s.name+".otf", s.style); err != nil {
+			panic(err)
+		}
+
+		c := canvas.New(210, 297)
+		ctx := canvas.NewContext(c)
+
+		size := 30.0
+		// y := 297.0
+		// for y, i := 297.0, 20; y > 0 && i > 0; i-- {
+		face := font.Face(size, canvas.Black, s.style, canvas.FontNormal)
+
+		txt := canvas.NewTextBox(face, s.name+lorem, 210.0, 0.0, canvas.Left, canvas.Top, 0.0, 0.0)
 		ctx.DrawText(0, y, txt)
 
 		y -= txt.Bounds().H
-		size -= 1.0
+		size -= 1.5
+		// }
+
+		c.RenderTo(pdf)
+		// pdf.NewPage(210, 297)
 	}
-	if err := renderers.Write("test.pdf", c, &pdf.Options{}); err != nil {
+
+	if err := pdf.Close(); err != nil {
 		panic(err)
 	}
 }
