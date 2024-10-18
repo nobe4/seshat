98d0d059b9986c7ea500e8a0b37d5af1c19be725
nobe4 <nobe4@users.noreply.github.com>
2024-10-18 19:13:22 +0200

    chore: drop alphabet and lorem testers
    Those can be moved to the `text` tester.

--- internal/testers/testers.go
+++ internal/testers/testers.go
@@ -1,10 +1,10 @@
 package testers
 
 import (
+	"fmt"
+
 	"github.com/nobe4/seshat/internal/font"
-	"github.com/nobe4/seshat/internal/testers/alphabet"
 	"github.com/nobe4/seshat/internal/testers/grid"
-	"github.com/nobe4/seshat/internal/testers/lorem"
 	"github.com/nobe4/seshat/internal/testers/text"
 	"github.com/tdewolff/canvas/renderers/pdf"
 )
@@ -13,15 +13,12 @@ type TestFn func(*pdf.PDF, font.Fonts, string, []string)
 
 func Get(name string) TestFn {
 	switch name {
-	case "alphabet":
-		return alphabet.Test
-	case "lorem":
-		return lorem.Test
 	case "grid":
 		return grid.Test
 	case "text":
 		return text.Test
 	}
 
-	panic("undefined tester " + name)
+	fmt.Printf("Unknown tester %s\n", name)
+	return nil
 }
