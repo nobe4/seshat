21f6f4e87defee65ff5c6654d4b995b529609ab8
nobe4 <nobe4@users.noreply.github.com>
2024-10-24 09:03:09 +0200

    feat(responsive): add responsiveness for the text (#35)

--- internal/testers/text/text.go
+++ internal/testers/text/text.go
@@ -8,6 +8,11 @@ import (
 )
 
 func Test(pdf *pdf.PDF, fonts font.Fonts, config config.Config, rule config.Rule) {
+	if rule.Args.Responsive {
+		testResponsive(pdf, fonts, config, rule)
+		return
+	}
+
 	width, height := pdf.Size()
 
 	y := height
@@ -40,4 +45,60 @@ func Test(pdf *pdf.PDF, fonts font.Fonts, config config.Config, rule config.Rule
 	c.RenderTo(pdf)
 
 	pdf.NewPage(width, height)
+}
+
+func testResponsive(pdf *pdf.PDF, fonts font.Fonts, config config.Config, rule config.Rule) {
+	width, height := pdf.Size()
+
+	y := height
+	c := canvas.New(width, height)
+	ctx := canvas.NewContext(c)
+
+	for _, input := range rule.Inputs {
+		for _, font := range fonts {
+
+			size := findFontSize(font, input, rule)
+
+			face := font.Font.Face(size, canvas.Black)
+			face.Font.SetFeatures(rule.Args.Features)
+
+			text := canvas.NewTextLine(face, input, canvas.Left)
+
+			nextY := y - text.Bounds().H
+
+			if nextY < 0 {
+				c.RenderTo(pdf)
+				c = canvas.New(width, height)
+				ctx = canvas.NewContext(c)
+				pdf.NewPage(width, height)
+
+				y = height
+			}
+
+			y -= text.Bounds().H
+
+			ctx.DrawText(0, y, text)
+		}
+	}
+
+	c.RenderTo(pdf)
+
+	pdf.NewPage(width, height)
+}
+
+func findFontSize(font font.Font, input string, rule config.Rule) float64 {
+	lastValidSize := 0.0
+
+	for size := 1.0; ; size += 1.0 {
+		face := font.Font.Face(size, canvas.Black)
+		face.Font.SetFeatures(rule.Args.Features)
+
+		txt := canvas.NewTextBox(face, input, rule.Args.Width, 0.0, canvas.Left, canvas.Top, 0.0, 0.0)
+
+		if txt.Bounds().W > rule.Args.Width-10 || txt.Bounds().H >= rule.Args.Height-10 {
+			return lastValidSize
+		}
+
+		lastValidSize = size
+	}
 }
